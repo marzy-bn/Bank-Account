7589101851d1efbe252cf7e5ca94f1427ee02776
Marzieh Babaeianjelodar <marzy_bn@Marziehs-MacBook-Air.local>
2024-09-09 17:48:20 -0400

    Creating a Bank Account project

---
+++ bank_accounts.py
@@ -0,0 +1,44 @@
+class BalanceException(Exception):
+    pass
+
+class BankAccount:
+    def __init__(self, initialAmount, acctName):
+        self.balance = initialAmount
+        self.name = acctName
+        print(f"\nAccount '{self.name}' created. \n Balance = ${self.balance:.2f}")
+    
+    def getBalance(self):
+        print(f"\nAccount '{self.name}' Balance = ${self.balance:.2f}")
+    
+    def deposit(self,amount):
+        self.balance = self.balance + amount
+        print("\nDeposit complete.")
+        self.getBalance()
+    
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return 
+        else:
+            raise BalanceException(f"\nSorry, account '{self.name}' only has a balance of ${self.balance:.2f}")
+    
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount
+            print("\n Withdraw complete.")
+            self.getBalance()
+        except BalanceException as error:
+            print(f'\nWithdraw interrupted: {error}')
+    
+    def transfer(self, amount, account):
+        try:
+            print('\n***********\n\nBeginning Transfer..  ')
+            self.viableTransaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
+            print('\nTransfer complete! \n***********')
+        except BalanceException as error:
+            print(f'\nTranfer interrupted. {error}')
+
+
+    
