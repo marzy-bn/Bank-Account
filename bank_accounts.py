f166b7190c6f4e56f7453892aa57521967bc6c9c
Marzieh Babaeianjelodar <marzy_bn@Marziehs-MacBook-Air.local>
2025-03-23 13:30:45 -0400

    Added inheritance

--- bank_accounts.py
+++ bank_accounts.py
@@ -16,7 +16,24 @@ class BankAccount:
 
     def get_balance(self):
         return self.__balance
+
+class BankOfAmerica(BankAccount):
+    def __init__(self, name, balance, interest_rate):
+        self.interest_rate = interest_rate
+        super().__init__(name, balance)
     
+    def get_interest_amount(self, duration):
+        interest_amount = super().get_balance() * (self.interest_rate / 100) * duration
+        return interest_amount        
+
+#class BOALoan(BankOfAmerica):
+#    def __init__(self, )
+
+obj = BankOfAmerica('Tom',10000, 2)
+amount = obj.get_interest_amount(2)
+print(amount)
+
+"""
 num = BankAccount('Tom',10000)
 num.deposit(1000)
 print(num.get_balance())
@@ -26,3 +43,4 @@ print(num.__dict__)
 num.name = 'Steve'
 print(num.__dict__)
 print(num.get_balance())
+"""
